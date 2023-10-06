aed08b46c3a9a061bce01b517d7c659df35b3900
NatashaMoses <117117440+NatashaMoses@users.noreply.github.com>
2023-10-06 15:46:12 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,33 +10,33 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/pinkbg.webp";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "pink background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a B.Sc student studyinng Mathematics and Computer Science at Jomo Kenyatta Universeity. I enjoy exploring and analyzing data to be used in AI.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
+  "Data Science",
+  "Artificial Intelligence",
+  "Machine Learning",
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Back-end development",
+  "Deep Learning",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about using my tech skills to solve real-world problems. By leveraging my tech experience, I am always learning ways to make tech accessible to all.";
 
 const About = () => {
   return (
